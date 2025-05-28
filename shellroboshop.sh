f6e716233dbbdc078666c053f031cd565023c239
vishnusai34 <vishnu.karimisetty@gmail.com>
2025-05-28 17:10:43 +0530

    .

--- shellroboshop.sh
+++ shellroboshop.sh
@@ -2,7 +2,7 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0d21fdc42084c4a5b" # replace with your SG ID
-INSTANCES=("mongodb" "catalogue" "frontend")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z06250553GOH005C2MYU6" # replace with your ZONE ID
 DOMAIN_NAME="vaws84s.site" # replace with your domain
 
